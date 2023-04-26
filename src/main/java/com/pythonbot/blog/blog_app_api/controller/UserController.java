8f150c1ab25b21e395644e48cbf7343f66c3b3d4
Ashish <anshm9448@gmail.com>
2023-04-26 07:52:56 +0530

    Completed User Api

--- src/main/java/com/pythonbot/blog/blog_app_api/controller/UserController.java
+++ src/main/java/com/pythonbot/blog/blog_app_api/controller/UserController.java
@@ -1,13 +1,15 @@
 package com.pythonbot.blog.blog_app_api.controller;
 
 import com.pythonbot.blog.blog_app_api.payloads.UserDto;
+import com.pythonbot.blog.blog_app_api.response.ApiResponse;
 import com.pythonbot.blog.blog_app_api.services.UserService;
-import com.pythonbot.blog.blog_app_api.services.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
@@ -20,13 +22,35 @@ public class UserController {
         UserDto createUser= this.userService.createUser(userDto);
         return  new ResponseEntity<>(createUser, HttpStatus.CREATED);
     }
-    //2.56.16
-   public  ResponseEntity<UserDto> updateUser(@PathVariable  userId,@RequestBody UserDto userDto ){
-        UserDto updateuser= this.userService.updateUser(userId, userDto);
-   }
+
     //put
+    @PutMapping("/{userId}")
+   public  ResponseEntity<UserDto> updateUser(@PathVariable Integer userId, @RequestBody UserDto userDto ){
+       UserDto updateuser= this.userService.updateUser( userDto, userId);
+       return ResponseEntity.ok(updateuser);
+
+   }
+    //delete
+   @DeleteMapping("/{userId}")
+   public ResponseEntity<ApiResponse> deleteuser(@PathVariable Integer userId){
+        this.userService.deleteUser(userId);
+        return new ResponseEntity<ApiResponse>(new ApiResponse("User deleted Succesfully", true), HttpStatus.OK);
+   }
+   //Get
+    @GetMapping("/")
+    public  ResponseEntity<List<UserDto>> getAllUser(){
+        return  ResponseEntity.ok(this.userService.getAllUser());
+    }
+    //GetByUserId
+    @GetMapping("/{userID}")
+    public  ResponseEntity<UserDto> getUserById(@PathVariable Integer userID){
+        return ResponseEntity.ok(this.userService.getUserBYiD(userID));
+    }
+
+
+
     //get
     //getbyId
-    //delete
+
 
 }
---
+++ src/main/java/com/pythonbot/blog/blog_app_api/response/ApiResponse.java
@@ -0,0 +1,15 @@
+package com.pythonbot.blog.blog_app_api.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse {
+    private String messega;
+    private boolean succes;
+}
