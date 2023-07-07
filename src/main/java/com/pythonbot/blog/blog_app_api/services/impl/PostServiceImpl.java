d3f75525e25e375ec2417cfff0661fcd0fd1fa83
Ashish <anshm9448@gmail.com>
2023-07-07 22:03:06 +0530

    security

--- src/main/java/com/pythonbot/blog/blog_app_api/services/impl/PostServiceImpl.java
+++ src/main/java/com/pythonbot/blog/blog_app_api/services/impl/PostServiceImpl.java
@@ -12,9 +12,6 @@ import com.pythonbot.blog.blog_app_api.repository.UserRepo;
 import com.pythonbot.blog.blog_app_api.services.PostService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -67,10 +64,7 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public List<PostDto> getAllPost() {
-        //int pageSize=5;
-        //int pageNumber=1;
-       // Pageable p = PageRequest.of(pageNumber,pageSize);
-       // Page<Post> postPage= this.postRepo.findAll(p);
+
         List<Post> postList= this.postRepo.findAll();
         List<PostDto> postDtoList= postList.stream().map((post)->this.modelMapper.map(post, PostDto.class)).collect(Collectors.toList());
         return  postDtoList;
@@ -103,7 +97,6 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public List<PostDto> searchPosts(String keyword) {
-
         return null;
     }
 }
